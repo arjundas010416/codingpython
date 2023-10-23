ba8a02836ab6d0b82980723134996528612e541b
Arjun Das TP <arjundastp3@gmail.com>
2023-10-23 21:59:59 +0530

    commit logical errors are there

--- problem1.py
+++ problem1.py
@@ -1,2 +1,23 @@
-#finding Leaders from list
-""" njfnjndskndfkj """
+
+
+list1=[int(x) for x in input().split()]
+
+limit=len(list1)
+
+i=0;leaders=[];flag=0
+while i<limit-1:
+    first=list1[i]
+    for k in range(i+1,limit-1):
+        if list1[k]>first:
+            break
+    else:
+        flag=1
+    
+    if flag==1:
+        leaders.append(first)
+    i=i+1
+
+        
+
+leaders.append(list1[limit-1])
+print(leaders)
